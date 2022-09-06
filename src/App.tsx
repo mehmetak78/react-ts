dbc5fa6cafa30247a886de7ec987abc7de350625
mehmetak <mehmetak78@hotmail.com>
2022-09-06 21:25:54 +0300

    initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+
+import Todos from "./components/Todos";
+import Todo from "./models/Todo";
+import NewTodo from "./components/NewTodo";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+
+    const addTodoHandler = (todoText: string) => {
+        const newTodo = new Todo(todoText);
+        setTodos((prevTodos) => {
+            return prevTodos.concat(newTodo);
+        })
+    }
+    
+    return (
+        <div>
+            <NewTodo onAddTodo={addTodoHandler} />
+            <Todos items={todos}/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, {useRef} from 'react';
+
+const NewTodo: React.FC<{onAddTodo: (enteredText: string) => void}> = (props) => {
+    
+    const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+    const submitHandler = (event: React.FormEvent) => {
+        event.preventDefault();
+        const enteredText = todoTextInputRef.current!.value;
+        if (enteredText.trim().length === 0) {
+            return;
+        }
+
+        props.onAddTodo(enteredText);
+    }
+
+    return (
+        <form onSubmit={submitHandler}>
+            <label htmlFor="text">Todo Text</label>
+            <input type="text" id={"text"} ref={todoTextInputRef}/>
+            <button>Add Todo</button>
+        </form>
+    );
+};
+
+export default NewTodo;
